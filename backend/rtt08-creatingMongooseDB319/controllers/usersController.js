aa6910d077b26c858b258d343de9b76c322db203
JcTheSavior <jacobconnell3@gmail.com>
2024-05-21 11:29:48 -0400

    Add; usersController for controlling CRUD for user

--- backend/rtt08-creatingMongooseDB319/controllers/usersController.js
+++ backend/rtt08-creatingMongooseDB319/controllers/usersController.js
@@ -2,7 +2,23 @@ const User = require('../models/user')
 
 const bcrypt = require('bcryptjs')
 // Encrypt Password? ---> use bcrypt
+const jwt = require('jsonwebtoken')
 // Sign-up 
+
+const signup = async(req, res) => {
+// 1. Get Email and Password - [req.body]
+    const {email, password } = req.body;
+
+    //** Hash Password {use bcryptjs} **/
+
+// 2. Create User
+   await User.create({
+        email, 
+        password,
+    });
+    console.log('User Created')
+// Send Response
+}
 
 // Login
 
